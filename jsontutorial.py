5c15f7cf94994cd5b26f1312c84cd0585519f0b8
Kyla <kflmorcillos@gmail.com>
2023-09-26 14:59:33 +0800

    Change code structure and add function for reading actions

--- jsontutorial.py
+++ jsontutorial.py
@@ -1,23 +1,55 @@
 import json
 
-try:
+
+def main():
+    print("1. Add actions\n2. Read actions")
+    choice = (int(input("\nEnter choice: ")))
+
+    if (choice == 1):
+        add_actions()
+    elif (choice == 2):
+        user_id = "1085820194334703666"
+        read_actions(user_id)
+
+
+def read_actions(user_id):
     with open("data.json", "r") as file:
         data = json.load(file)
-except FileNotFoundError:
-    data = {}
 
-user_id = input("User ID: ")
+    user_data = data.get(user_id, {})
+    total_score = user_data.get("total_score", 0)
+    actions = user_data.get("actions", {})
 
-if user_id not in data:
-    data[user_id] = {
-        "total_score": 100,
-        "actions": {}
-    }
+    print(f"User ID: {user_id}")
+    print(f"Total Score: {total_score}")
+    print("Actions:")
+    for action, points in actions.items():
+        print(f"{action}    {points}")
 
-action = input("Action: ")
-points = int(input("Points: "))
 
-data[user_id]["actions"][action] = points
+def add_actions():
+    try:
+        with open("data.json", "r") as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        data = {}
 
-with open("data.json", "w") as file:
-    json.dump(data, file, indent=4)
+    user_id = input("User ID: ")
+
+    if user_id not in data:
+        data[user_id] = {
+            "total_score": 100,
+            "actions": {}
+        }
+
+    action = input("Action: ")
+    points = int(input("Points: "))
+
+    data[user_id]["actions"][action] = points
+
+    with open("data.json", "w") as file:
+        json.dump(data, file, indent=4)
+
+
+if __name__ == "__main__":
+    main()
