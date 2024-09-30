3f3fc5b2e38aab4c695160351c9d2c248ae989df
Kyla <kflmorcillos@gmail.com>
2024-09-30 17:15:40 +0800

    Create questions array

--- 01 - Quiz Game/main.py
+++ 01 - Quiz Game/main.py
@@ -1,6 +1,12 @@
 from data import questions_data
+from question import Question
+
+questions = []
 
 for question in questions_data:
-    print(question["question"])
-    print("\n".join(question["choices"]))
-    print()
+    question_text = question["question"]
+    question_choices = question["choices"]
+    question_answers = question["answer"]
+
+    new_question = Question(question_text, question_choices, question_answers)
+    questions.append(new_question)
