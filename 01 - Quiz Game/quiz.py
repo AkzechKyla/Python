622a9be15c3859f69ac18923a79bd2e40fd1f769
Kyla <kflmorcillos@gmail.com>
2024-10-09 19:20:37 +0800

    Check if quiz still has questions left

--- 01 - Quiz Game/quiz.py
+++ 01 - Quiz Game/quiz.py
@@ -1,6 +1,10 @@
 class Quiz:
     def __init__(self, questions):
+        self.question_number = 0
         self.questions = questions
+
+    def still_has_questions(self):
+        return self.question_number < len(self.questions)
 
     def test(self):
         for question in self.questions:
