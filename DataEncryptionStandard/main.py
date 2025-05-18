43675ec9f980ea6484e83d95dfcafb205cf7a9dc
Kyla <kflmorcillos@gmail.com>
2025-05-19 01:52:18 +0800

    Refactor step 3

--- DataEncryptionStandard/main.py
+++ DataEncryptionStandard/main.py
@@ -28,9 +28,30 @@ def permute_choice_1(bin_str):
     # Permute the binary string according to PC-1
     return "".join(bin_str[i - 1] for i in permuted_choice_1)
 
+
 def left_shift(key_half, shifts):
     # Perform left shift on the key half
     return key_half[shifts:] + key_half[:shifts]
+
+
+def generate_subkeys(c0, d0):
+    c_list = []
+    d_list = []
+
+    for i in range(1, 17):
+        shift = 1 if i in [1, 2, 9, 16] else 2
+        c = left_shift(c0, shift)
+        d = left_shift(d0, shift)
+
+        # Print for visualization
+        print("C{} = {}".format(i, c0))
+        print("D{} = {}".format(i, d0))
+        print()
+
+        c_list.append(c)
+        d_list.append(d)
+
+    return c_list, d_list
 
 
 print("Input Values:")
@@ -43,24 +64,16 @@ K_bin = hex_to_bin(K)
 K_blocks = bin_to_4bit_blocks(K_bin)
 print("K = ", K_bin)
 print(K_blocks)
-print()
 
-print("Step 1: Permute the 64-bit key (K_bin) into a 56-bit key using PC-1")
+print("\nStep 1: Permute the 64-bit key (K_bin) into a 56-bit key using PC-1")
 pc1_key = permute_choice_1(K_bin)
 print("K += ", pc1_key)
 
-print("Step 2: Split the 56-bit key into two 28-bit halves, C0 and D0")
+print("\nStep 2: Split the 56-bit key into two 28-bit halves, C0 and D0")
 c0 = pc1_key[:28]
 d0 = pc1_key[28:]
 print("C0 = ", c0)
 print("D0 = ", d0)
 
-print("Step 3: Create 16 block Cn and Dn (1<=n<=16) using left shift")
-
-for i in range(1, 17):
-    shift = 1 if i in [1, 2, 9, 16] else 2
-    c0 = left_shift(c0, shift)
-    d0 = left_shift(d0, shift)
-    print("C{} = ".format(i), c0)
-    print("D{} = ".format(i), d0)
-    print()
+print("\nStep 3: Create 16 block Cn and Dn (1<=n<=16) using left shift")
+c_list, d_list = generate_subkeys(c0, d0)
